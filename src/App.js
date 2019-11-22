83dab5c8180a78f051b8b5dd8eb0aa42fa68439c
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-22 16:21:32 -0500

    ndf;asdfs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import craiglist from "./craiglist.json";
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import map from "./map.jpg"
 
 export default class App extends Component {
   state = {
@@ -40,12 +41,39 @@ export default class App extends Component {
 
         <div className="container">
           <div className="row">
-            <span className="col-3">search bar</span>
+            <span className="col-3" text-align="center">
+              <div>Housing</div>
+              <div>
+
+              <input type="checkbox" label="pets"/> Pets
+
+              </div>
+              <div>
+
+              <input type="checkbox" label="pets"/> Furnished
+
+              </div>
+              <div>
+
+              <input type="checkbox" label="pets"/> Non-Smoking
+
+              </div>
+
+            <form>
+              <input className=""/>
+              
+              
+            </form>
+            
+            
+            </span>
             <span className="col-5">
               helloo
               <ui className="list-group">{this.getObjects()}</ui>
             </span>
-            <span className="col-4">map</span>
+            <span className="col-4">
+              <img src={map} alt="blah" image-size="cover"></img>
+            </span>
           </div>
         </div>
       </div>
